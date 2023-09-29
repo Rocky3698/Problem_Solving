31998ac193fa925a77588095653ba69b74f94ffd
Rocky Chowdhury <rocky20809@gmail.com>
2023-09-30 02:17:24 +0600

    basic

---
+++ Implementation_problems/A_Multiplication_1.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+#define For(i, s, n) for (int i = s; i < n; i++)
+typedef pair<int, int> pii;
+const int INF = 1e9 + 7;
+const int N = 1e5 + 5;
+const int M = 1e3 + 5;
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    return 0;
+}
